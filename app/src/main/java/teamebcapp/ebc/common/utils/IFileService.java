d86cb01c14d72436bd54ae17ede7719b74563650
devwoong <chl_woong159@naver.com>
2019-02-09 22:54:50 +0900

    파일업로드 서비스 추가(테스트전)

--- app/src/main/java/teamebcapp/ebc/common/utils/IFileService.java
+++ app/src/main/java/teamebcapp/ebc/common/utils/IFileService.java
@@ -1,4 +1,21 @@
 package teamebcapp.ebc.common.utils;
 
-public class IFileService {
+import java.util.Map;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+import retrofit2.http.PartMap;
+import retrofit2.http.Path;
+
+public interface IFileService {
+    @Multipart
+    @POST("/{urls}")
+    Call<Map<String, Object>> uploadImage(
+            @Path("urls") String urls,
+            @Part MultipartBody.Part File,
+            @PartMap Map<String, RequestBody> params);
 }
