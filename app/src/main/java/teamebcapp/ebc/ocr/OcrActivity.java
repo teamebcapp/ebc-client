ad3f6f386457f4c5860758c5dafe1d8462ce4a13
devwoong <chl_woong159@naver.com>
2019-02-19 22:11:57 +0900

    ocr추가

---
+++ app/src/main/java/teamebcapp/ebc/ocr/OcrActivity.java
@@ -0,0 +1,13 @@
+package teamebcapp.ebc.ocr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OcrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ocr);
+    }
+}
